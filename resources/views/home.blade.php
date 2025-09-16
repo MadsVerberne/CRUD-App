bb924c6fa0449ecb39c5f89cec08217e1aed909b
Bjorn Foederer <your-email@example.com>
2025-09-16 12:42:51 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Home - CRUD App</title>
 </head>
+
 <body>
     <h1>Homepagina</h1>
+    <div>123456</div>
     <a href="/about">About</a>
 </body>
+
 </html>
