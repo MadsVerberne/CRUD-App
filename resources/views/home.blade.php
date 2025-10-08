d3d62784f6f464388a08f7f8abfd6e9f262c3e3a
Bjorn Foederer <your-email@example.com>
2025-10-08 11:14:08 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,20 +7,14 @@
 </head>
 
 <body>
-<<<<<<< HEAD
-    <h1>Homepagina</h1>
-    <div>123456</div>
-    <a href="/about">About</a>
-=======
     <header>
         <h1>CRUD APP</h1>
         <a href="/">Home</a>
         <a href="/about">About</a>
     </header>
     <main>
-        
+
     </main>
->>>>>>> 4998f56 (Week 3: Home met styling)
 </body>
 
 </html>
